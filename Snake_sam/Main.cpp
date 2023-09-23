f76a21648355b61968a7985d5db0b4d42442ba37
sbazu <sbazu@SASHA>
2023-09-23 16:21:33 +0300

    Добавьте файлы проекта.

---
+++ Snake_sam/Main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+const short WIDTH = 17;
+const short HIGHT = 10;
+
+bool isRunning = true;	//Главная логическая переменная 
+
+char map[] = {
+	"###############\n"
+	"#             #\n"
+	"#             #\n"
+	"#             #\n"
+	"#             #\n"
+	"#             #\n"
+	"#             #\n"
+	"#             #\n"
+	"#             #\n"
+	"###############\n"
+}
+	
+
+int main() {
+	while (isRunning) {
+
+	}
+}
