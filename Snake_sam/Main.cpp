8d0f3780c2ba483607a9cdd8270eed1ebb35f7de
sbazu <zvezdopda659527@gmail.com>
2023-09-24 15:54:43 +0300

    Поменяла голову змейке и добавила комментарии в код

--- Snake_sam/Main.cpp
+++ Snake_sam/Main.cpp
@@ -11,7 +11,7 @@ const short RIGHT = 2;
 const short DOWN = 3;
 
 
-bool isRunning = true;	//Главная логическая переменная 
+bool isRunning = true;	//Главная логическая переменная для игры
 
 char map[] = {
 	"###############\n"
@@ -27,8 +27,12 @@ char map[] = {
 };
 
 //SNAKE
-short snakeLen = 1;
-char snakeHead = '0';
+short snakeLen = 4;
+char snakeUp = '^';
+char snakeLeft = '<';
+char snakeDown = 'v';
+char snakeRigth = '>';
+char snakeHead = snakeUp;
 char snakeBody = 'O';
 
 short snakeDir = UP;
@@ -36,6 +40,7 @@ short snakeDir = UP;
 int snakeX[MAX_SNAKE_LEN] = { WIDTH / 2 };
 int snakeY[MAX_SNAKE_LEN] = { HIGHT / 2 };
 
+//Метод для переправления курсора по координатам X и Y
 int gotoxy(short x, short y) {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	COORD pos = { x, y };
@@ -46,56 +51,68 @@ int gotoxy(short x, short y) {
 
 int main() {
 	int time = clock();
-	while (isRunning) {
+	while (isRunning) {	//Главный цикл игры
 
+		//Постоянная ассинхронная проверка на нажатие кнопок клавиатуры и изменение направления движения змейки
 		if (GetKeyState('A') & 0x8000) {
 			if (snakeLen == 1) {
 				snakeDir = LEFT;
+				snakeHead = snakeLeft;
 			}
 			else if (snakeDir != RIGHT) {
 				snakeDir = LEFT;
+				snakeHead = snakeLeft;
 			}
 		}
 		if (GetKeyState('W') & 0x8000) {
 			if (snakeLen == 1) {
 				snakeDir = UP;
+				snakeHead = snakeUp;
 			}
 			else if (snakeDir != DOWN) {
 				snakeDir = UP;
+				snakeHead = snakeUp;
 			}
 		}
 		if (GetKeyState('D') & 0x8000) {
 			if (snakeLen == 1) {
 				snakeDir = RIGHT;
+				snakeHead = snakeRigth;
 			}
 			else if (snakeDir != LEFT) {
 				snakeDir = RIGHT;
+				snakeHead = snakeRigth;
 			}
 		}
 		if (GetKeyState('S') & 0x8000) {
 			if (snakeLen == 1) {
 				snakeDir = DOWN;
+				snakeHead = snakeDown;
 			}
 			else if (snakeDir != UP) {
 				snakeDir = DOWN;
+				snakeHead = snakeDown;
 			}
 		}
 
+		//Отрисовка карты через каждую секунду
 		if ((clock() - time) / CLOCKS_PER_SEC >= 1) {	
 			time = clock();
 			gotoxy(0, 0);
 			std::cout << "~ length: " << snakeLen << std::endl;
 
+			//Координаты тела змейки
 			for (short i = snakeLen; i >= 0; --i) {
 				snakeX[i + 1] = snakeX[i];
 				snakeY[i + 1] = snakeY[i];
 			}
-
+			//Перемещение змейки
 			if (snakeDir == LEFT) snakeX[0]--;
 			if (snakeDir == UP) snakeY[0]--;
 			if (snakeDir == RIGHT) snakeX[0]++;
 			if (snakeDir == DOWN) snakeY[0]++;
 			
+			//Отрисовка змейки на карте
 			for (short i = 0; i < snakeLen; i++) {
 				if (i == 0) {
 					map[snakeY[i] * WIDTH + snakeX[i]] = snakeHead;
@@ -106,8 +123,10 @@ int main() {
 				std::cout << "element " << i << " is on " << snakeY[i] * WIDTH + snakeX[i] << std::endl;
 			}
 
+			//Отрисовка карты
 			std::cout << map;
 
+			//Очищение карты
 			for (short i = 0; i < snakeLen; i++) {
 				map[snakeY[i] * WIDTH + snakeX[i]] = ' ';
 			}
